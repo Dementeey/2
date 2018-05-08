e9409d22bb9833453580ff7853892f602646547f
dementeey-Laptop <metallist9656@gmail.com>
2018-05-08 08:59:50 +0300

    finised

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,33 @@
-function toggleClass() {
-  var burger = document.querySelector('.burger-wrap')
-  var pageNav = document.querySelector('.page-nav')
-  burger.classList.toggle('burger-wrap--active')
-  pageNav.classList.toggle('page-nav--active')
+// function toggleClass() {
+//   var burger = document.querySelector('.burger-wrap')
+//   var pageNav = document.querySelector('.page-nav')
+//   burger.classList.toggle('burger-wrap--active')
+//   pageNav.classList.toggle('page-nav--active')
+// }
+
+
+var vid = document.querySelector('.testimonial__video');
+var pauseBtn = document.querySelector('.testimonial__video-pause-btn');
+var playBtn = document.querySelector('.testimonial__video-play-btn');
+
+function videoPlayBtn() {
+  if (vid.paused) {
+    vid.play();
+    pauseBtn.style.display = 'block'
+
+  }
+  if (vid.play()) {
+    playBtn.style.display = 'none'
+  }
+}
+
+function videoPauseBtn() {
+  if (!vid.paused) {
+    vid.pause();
+    playBtn.style.display = 'block'
+  }
+
+  if (vid.paused) {
+    pauseBtn.style.display = 'none'
+  }
 }
